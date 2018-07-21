0b6b643b82c449ceafea0d25627310cec71208af
AddisonPascal <addisondj@hotmail.com>
2018-07-21 13:58:08 +1000

    Added coordinate display
    Displays playing area

--- Game.bat
+++ Game.bat
@@ -34,3 +34,12 @@ goto playing
 
 :playing
 cls
+echo  -----
+echo ^|%coord.1.1%%coord.1.2%%coord.1.3%%coord.1.4%%coord.1.5%^|
+echo ^|%coord.2.1%%coord.2.2%%coord.2.3%%coord.2.4%%coord.2.5%^|
+echo ^|%coord.3.1%%coord.3.2%%coord.3.3%%coord.3.4%%coord.3.5%^|
+echo ^|%coord.4.1%%coord.4.2%%coord.4.3%%coord.4.4%%coord.4.5%^|
+echo ^|%coord.5.1%%coord.5.2%%coord.5.3%%coord.5.4%%coord.5.5%^|
+echo  -----
+choice /c 0wasd /n /t 1 /d 0 >nul
+goto playing
