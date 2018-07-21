58aa79445ea1700226fd383f2b4350806724f133
AddisonPascal <addisondj@hotmail.com>
2018-07-21 11:32:59 +1000

    Added some code
    Added about four fifths of the coordinate setting code.

---
+++ Game.bat
@@ -0,0 +1,28 @@
+@echo off
+title Game!
+mode 1000
+goto start
+
+:start
+cls
+set coord.1.1=
+set coord.1.2=
+set coord.1.3=
+set coord.1.4=
+set coord.1.5=
+set coord.2.1=
+set coord.2.2=
+set coord.2.3=
+set coord.2.4=
+set coord.2.5=
+set coord.3.1=
+set coord.3.2=
+set coord.3.3=
+set coord.3.4=
+set coord.3.5=
+set coord.4.1=
+set coord.4.2=
+set coord.4.3=
+set coord.4.4=
+set coord.4.5=
+set coord.5.1=
