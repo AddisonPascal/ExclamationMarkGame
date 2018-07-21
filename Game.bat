45e0825b9dd6e3b0a102af14d5b2d3aed24e205e
AddisonPascal <addisondj@hotmail.com>
2018-07-21 14:18:51 +1000

    Added moving!
    The player can now move using the w/a/s/d keys!

--- Game.bat
+++ Game.bat
@@ -30,9 +30,13 @@ set coord.5.2=
 set coord.5.3= 
 set coord.5.4= 
 set coord.5.5= 
+set yCoord=3
+set xCoord=3
 goto playing
 
 :playing
+set originalYCoord=%yCoord%
+set originalXCoord=%xCoord%
 cls
 echo  -----
 echo ^|%coord.1.1%%coord.1.2%%coord.1.3%%coord.1.4%%coord.1.5%^|
@@ -41,5 +45,41 @@ echo ^|%coord.3.1%%coord.3.2%%coord.3.3%%coord.3.4%%coord.3.5%^|
 echo ^|%coord.4.1%%coord.4.2%%coord.4.3%%coord.4.4%%coord.4.5%^|
 echo ^|%coord.5.1%%coord.5.2%%coord.5.3%%coord.5.4%%coord.5.5%^|
 echo  -----
-choice /c 0wasd /n /t 1 /d 0 >nul
+choice /c 0wasd /t 1 /d 0 >nul
+if %ERRORLEVEL%==1 goto gameTick
+if %ERRORLEVEL%==2 goto moveForward
+if %ERRORLEVEL%==3 goto moveLeft
+if %ERRORLEVEL%==4 goto moveBackward
+if %ERRORLEVEL%==5 goto moveRight
+goto playing
+
+:moveForward
+set /a yCoord=%yCoord%-1
+if %yCoord%==0 set yCoord=1
+goto gameTick
+
+:moveLeft
+set /a xCoord=%xCoord%-1
+if %xCoord%==0 set xCoord=1
+goto gameTick
+
+:moveBackward
+set /a yCoord=%yCoord%+1
+if %yCoord%==6 set yCoord=5
+goto gameTick
+
+:moveRight
+set /a xCoord=%xCoord%+1
+if %xCoord%==0 set xCoord=1
+goto gameTick
+
+:gameTick
+(
+@echo off
+echo set coord.%originalYCoord%.%originalXCoord%= 
+echo set coord.%yCoord%.%xCoord%=+
+echo. 
+)>tmp.bat
+call tmp.bat
+del tmp.bat
 goto playing
